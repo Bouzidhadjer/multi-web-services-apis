bc08c37ad75d0552e059b4af65f0c7735b938c11
hadjerbzd <bouzidhadjer34@gmail.com>
2023-09-08 16:10:34 +0100

    update

--- src/main/java/com/example/customerdataservice/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customerdataservice.repository;
-
-import com.example.customerdataservice.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository  extends JpaRepository<Customer, Long> {
-}
